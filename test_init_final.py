b6c442388c8c7f70e5cb1c5ea48b8986eb5f13af
sikote <63118589+sikote@users.noreply.github.com>
2020-12-24 15:49:14 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -3333,14 +3333,14 @@ class IlsangDistributionBot(commands.AutoShardedBot):
 
 					################ 보스 멍 처리 ################ 
 
-					if message.content.startswith(bossData[i][0] +'멍') or message.content.startswith(bossData[i][0] +' 멍'):
+					if message.content.startswith(bossData[i][0] +'hajimahajima') or message.content.startswith(bossData[i][0] +' hajimahajima'):
 						if hello.find('  ') != -1 :
 							bossData[i][6] = hello[hello.find('  ')+2:]
 							hello = hello[:hello.find('  ')]
 						else:
 							bossData[i][6] = ''
 							
-						tmp_msg = bossData[i][0] +'멍'
+						tmp_msg = bossData[i][0] +'hajimahajima'
 						tmp_now = datetime.datetime.now() + datetime.timedelta(hours = int(basicSetting[0]))
 
 						if len(hello) > len(tmp_msg) + 3 :
